b2753ec792a7a757ccedd9965997d7eea0c3a6a4
Sebastian Buitrago <sebastian.buitrago@sourcemeridian.com>
2022-09-07 18:58:20 -0500

    working without code plitting

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,21 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-// import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
-
-// import { terser } from "rollup-plugin-terser";
-// const packageJson = require("./package.json");
+import dts from "rollup-plugin-dts";
 
 export default [
   {
-    input: [
-      "src/index.ts",
-      "src/components/atoms/index.ts",
-      "src/components/atoms/index.ts"
-    ],
+    input: "src/index.ts",
+
     output: [
       {
-        dir: "build",
+        file: "build/index.js",
         format: "esm",
         sourcemap: true
       }
     ],
-    preserveModules: true,
     plugins: [
       peerDepsExternal(),
       resolve(),
@@ -35,13 +28,13 @@ export default [
       postcss({
         extensions: [".css"]
       })
-    ],
-    external: ["react", "react-dom", /\.css$/]
+    ]
+  },
+
+  {
+    input: "build/index.d.ts",
+    output: [{ file: "build/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
   }
-  // {
-  // input: "build/index.d.ts",
-  // output: [{ file: "build/index.d.ts", format: "esm" }],
-  // plugins: [dts()],
-  // external: [/\.css$/]
-  // }
 ];
