fd5aa909faa19b59ce9bd6b54a9841b5a88aa80b
Sebastian Buitrago <sebastian.buitrago@sourcemeridian.com>
2022-09-07 15:06:36 -0500

    changes rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,58 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
-const packageJson = require('./package.json');
+// import { terser } from "rollup-plugin-terser";
+
+const packageJson = require("./package.json");
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
+        format: "cjs",
         sourcemap: true
       },
       {
         file: packageJson.module,
-        format: 'esm',
+        format: "esm",
         sourcemap: true
       }
     ],
+
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' })
-    ]
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        modules: true,
+
+        extract: false,
+        extensions: ["module.css"]
+
+        // modules: true
+      }),
+      postcss({
+        modules: false,
+        extract: false,
+        extensions: [".css"]
+
+        // modules: true
+      })
+      // terser()
+    ],
+    external: ["react", "react-dom"]
   },
   {
-    input: 'dist/esm/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
   }
 ];
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+// import * as styles from './Button.module.css';
 
+// import style from "./Button.css";
+
+// Inject to `<head>` and also available as `style`
+import "./global.css";
+import style from "./Button.module.css";
 export interface ButtonProps {
   label: string;
 }
@@ -7,13 +13,20 @@ export interface ButtonProps {
 const Button = (props: ButtonProps) => {
   const [count, setCount] = useState(0);
 
-  console.log('count change', count);
+  console.log("count change", count);
+
+  useEffect(() => {
+    console.log("useEffect", count);
+  }, [count]);
+
   return (
     <button
+      className={`${style.button} button-global`}
       onClick={() => {
         setCount((state) => state + 1);
       }}
     >
+      holaaa
       {props.label}
     </button>
   );
