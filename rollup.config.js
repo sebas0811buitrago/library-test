f73891175896bc975f22363b5f5875bcaff28e08
Sebastian Buitrago <sebastian.buitrago@sourcemeridian.com>
2022-09-07 18:45:04 -0500

    code spliting with errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,23 @@
 import resolve from "@rollup/plugin-node-resolve";
-// import commonjs from "@rollup/plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+// import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 
 // import { terser } from "rollup-plugin-terser";
-const packageJson = require("./package.json");
+// const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: [
+      "src/index.ts",
+      "src/components/atoms/index.ts",
+      "src/components/atoms/index.ts"
+    ],
     output: [
       {
-        dir: "dist",
+        dir: "build",
         format: "esm",
         sourcemap: true
       }
@@ -22,7 +26,7 @@ export default [
     plugins: [
       peerDepsExternal(),
       resolve(),
-      // commonjs(),
+      commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss({
         modules: true,
@@ -32,12 +36,12 @@ export default [
         extensions: [".css"]
       })
     ],
-    external: ["react", "react-dom"]
-  },
-  {
-    input: "dist/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/]
+    external: ["react", "react-dom", /\.css$/]
   }
+  // {
+  // input: "build/index.d.ts",
+  // output: [{ file: "build/index.d.ts", format: "esm" }],
+  // plugins: [dts()],
+  // external: [/\.css$/]
+  // }
 ];
